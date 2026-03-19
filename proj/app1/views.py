bf66e9d7057ccc521d5ab844eff1444d437a9afb
counter185 <33550839+counter185@users.noreply.github.com>
2026-03-19 13:20:43 +0100

    cw3

--- proj/app1/views.py
+++ proj/app1/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Task
 
 # Create your views here.
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'task_list.html'
+    context_object_name = 'tasks'
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'task_detail.html'
+    context_object_name = 'task'
